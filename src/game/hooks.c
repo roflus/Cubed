ba754374fcd74082329e78adb96a53f691bff3ed
Rolf Haring <rharing@f1r5s13.codam.nl>
2023-03-09 15:41:01 +0100

    mouse

--- src/game/hooks.c
+++ src/game/hooks.c
@@ -6,7 +6,7 @@
 /*   By: rharing <rharing@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/03 15:42:11 by rharing       #+#    #+#                 */
-/*   Updated: 2023/03/09 13:19:00 by rharing       ########   odam.nl         */
+/*   Updated: 2023/03/09 15:40:29 by rharing       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,12 +74,8 @@ static void	turn_left(t_vars *vars)
 		old_plane_x * sin(ROTSPEED) + vars->ray.plane_y * cos(ROTSPEED);
 }
 
-void	hook(void *param)
+void	keyhook(t_vars *vars)
 {
-	t_vars	*vars;
-
-	vars = param;
-	raycaster(vars);
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(vars->mlx);
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_UP))
@@ -94,4 +90,74 @@ void	hook(void *param)
 		strafe_right(vars);
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_A))
 		strafe_left(vars);
+	if (mlx_is_key_down(vars->mlx, MLX_KEY_W))
+		move_forward(vars);
+	if (mlx_is_key_down(vars->mlx, MLX_KEY_S))
+		move_backwards(vars);
+}
+
+void	mouse_look_right(t_vars *vars)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_plane_x = vars->ray.plane_x;
+	old_dir_x = vars->ray.dir_x;
+	vars->ray.dir_x = \
+		vars->ray.dir_x * cos(-ROTSPEED) - vars->ray.dir_y * sin(-ROTSPEED);
+	vars->ray.dir_y = \
+		old_dir_x * sin(-ROTSPEED) + vars->ray.dir_y * cos(-ROTSPEED);
+	vars->ray.plane_x = \
+		vars->ray.plane_x * cos(-ROTSPEED) - vars->ray.plane_y * sin(-ROTSPEED);
+	vars->ray.plane_y = \
+		old_plane_x * sin(-ROTSPEED) + vars->ray.plane_y * cos(-ROTSPEED);
+}
+
+void	mouse_look_left(t_vars *vars)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_plane_x = vars->ray.plane_x;
+	old_dir_x = vars->ray.dir_x;
+	vars->ray.dir_x = \
+		vars->ray.dir_x * cos(ROTSPEED) - vars->ray.dir_y * sin(ROTSPEED);
+	vars->ray.dir_y = \
+		old_dir_x * sin(ROTSPEED) + vars->ray.dir_y * cos(ROTSPEED);
+	vars->ray.plane_x = \
+		vars->ray.plane_x * cos(ROTSPEED) - vars->ray.plane_y * sin(ROTSPEED);
+	vars->ray.plane_y = \
+		old_plane_x * sin(ROTSPEED) + vars->ray.plane_y * cos(ROTSPEED);
+}
+
+void	mousehook(t_vars *vars)
+{
+	int	cur_x;
+	int	cur_y;
+	int	mouse_x;
+
+	cur_x = WIDTH / 2;
+	mouse_x = cur_x;
+	mlx_get_mouse_pos(vars->mlx, &cur_x, &cur_y);
+	mlx_set_cursor_mode(vars->mlx, MLX_MOUSE_HIDDEN);
+	if (cur_x != mouse_x)
+	{
+		if (cur_x > mouse_x)
+			mouse_look_right(vars);
+		else if (cur_x < mouse_x)
+			mouse_look_left(vars);
+	}
+	mlx_set_mouse_pos(vars->mlx, WIDTH / 2, HEIGHT / 2);
+	cur_x = WIDTH / 2;
+	mouse_x = cur_x;
+}
+
+void	hook(void *param)
+{
+	t_vars	*vars;
+
+	vars = param;
+	raycaster(vars);
+	keyhook(vars);
+	mousehook(vars);
 }
