6bea82d390f8a015b51eac8da26b840939af5fa2
Rolf Haring <rharing@f0r1s15.codam.nl>
2023-04-04 15:45:02 +0200

    calloc

--- src/parser/open_file.c
+++ src/parser/open_file.c
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   open_file.c                                        :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: qfrederi <qfrederi@student.42.fr>            +#+                     */
+/*   By: rharing <rharing@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/11 14:40:40 by rharing       #+#    #+#                 */
-/*   Updated: 2023/03/31 16:10:58 by qfrederi      ########   odam.nl         */
+/*   Updated: 2023/04/04 15:44:38 by rharing       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,7 @@ static void	init(t_vars *vars)
 
 	i = 0;
 	ft_memset(vars, 0, sizeof(vars));
-	vars->map.data = (char **)malloc(7 * sizeof(char *));
-	if (vars->map.data == NULL)
-		ft_error("Malloc failed", 1);
-	while (i < 7)
-	{
-		vars->map.data[i] = NULL;
-		i++;
-	}
+	vars->map.data = ft_calloc(7, sizeof(char *));
 	vars->mlx = mlx_init(WIDTH, HEIGHT, "Cub3D", false);
 	if (!vars->mlx)
 		exit(EXIT_FAILURE);
